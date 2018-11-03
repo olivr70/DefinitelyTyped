1cfdfac0f7e777f3a310c3b857dd2334445880cc
Olivier Chevet <olivr70@outlook.fr>
2018-11-03 19:56:36 +0100

    Correction of "string-template/compile" - "compile" instead of "comile" (missing p)

--- types/string-template/index.d.ts
+++ types/string-template/index.d.ts
@@ -13,13 +13,13 @@ declare namespace StringTemplate {
      * Format
      */
     interface Format {
-        // Format using an object hash with keys matching [0-9a-zA-Z]+ 
+        // Format using an object hash with keys matching [0-9a-zA-Z]+
         (string: string, object: any): string;
         // Format using a number indexed array
         (string: string, array: Array<any>): string;
-        // Format using optional arguments 
+        // Format using optional arguments
         (string: string, ...array: Array<any>): string;
-        // Escape {} pairs by using double {{}} 
+        // Escape {} pairs by using double {{}}
         (string: string): string;
     }
 
@@ -47,6 +47,6 @@ declare module "string-template" {
 }
 
 declare module "string-template/compile" {
-    const comile: StringTemplate.Compile;
-    export = comile;
+    const compile: StringTemplate.Compile;
+    export = compile;
 }
